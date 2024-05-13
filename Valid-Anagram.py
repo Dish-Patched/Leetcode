bf8718646f5e3ee57bb8a6d03a2cbd8625047de7
Dishanta Sarma <147008603+Dish-Patched@users.noreply.github.com>
2024-05-13 10:20:31 -0500

    Create Valid-Anagram.py

---
+++ Valid-Anagram.py
@@ -0,0 +1,7 @@
+from collections import defaultdict
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if sorted(s) == sorted(t):
+            return True
+        else:
+            return False
